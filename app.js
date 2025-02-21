490cdd53390d33dd48eb3571cf746c6b47af019c
Jack1812 <jackandypabloalvarado@gmail.com>
2025-02-21 18:17:07 -0500

    mostrar dos amigos

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ function actualizarLista() {
 
 // Función para seleccionar aleatoriamente un amigo secreto
 function sortearAmigo() {
-    // Verifica si la lista de amigos tiene al menos dos nombres 
+    // Verifica si la lista de amigos tiene al menos dos nombres para sortear
     if (listaAmigos.length < 2) {
         alert("Agregue al menos dos nombres antes de sortear.");
         return;
